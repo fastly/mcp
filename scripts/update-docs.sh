3342cf9f98e675b5e668156c86d1d7cc4043e5ff
Frank Denis <github@pureftpd.org>
2026-09-22 15:55:40 +0200

    update-docs: better regex

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -26,6 +26,6 @@ fi
 echo "Copying $count Api.md files to $DEST..."
 rm -rf "$DEST"
 cp -r docs "$DEST"
-find "$DEST" -type f -name '*.md' -exec perl -pi -e 's/[ \t]+$//' {} +
+find "$DEST" -type f -name '*.md' -exec perl -pi -e 's/[ \t]+(\r?)$/$1/' {} +
 
 echo "Done. Run 'bun test' to verify the index still builds."
